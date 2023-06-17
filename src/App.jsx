95e9539b87d68e4be3fade32c60a0703f291161b
JRMRIVAS <127703496+JRMRIVAS@users.noreply.github.com>
2023-06-17 11:28:21 -0600

    Modificacion de archivos con celeste

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './assets/css/style.css'
-
-
-
 function App() {
 
-    return (
-      <>
-        <Header />
-       
-        
-       
-      </>
-    )
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
 }
 
 export default App
