dfdf6ce60df6d1e71c4f246117d11fc45eacd8f7
celestefranco <mariacolocho2021@gmail.com>
2023-06-23 23:01:34 -0600

    Agregando Componente de Busquedas
    Sevagrega Búsqueda por nombre, estado, genero y Especie

---
+++ src/components/BusquedasEd.jsx
@@ -0,0 +1,114 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+
+export default function BusquedasEd() {
+    //asignando 2 estados
+    //un estado para todos los personajes y otro estado para un personaje en especifico por su nombre
+
+    const [personajes, setPersonajes] = useState([]);
+    const [name, setName] = useState("");
+    const [state, setState] = useState("");
+    const [specie, setSpecie] = useState("");
+    const [gender, setGender] = useState("");
+
+    const obtenerPersonajeByNombre = () => {
+        axios.get(`https://rickandmortyapi.com/api/character?name=${name}&status=${state}&species=${specie}&gender=${gender}`).then((response) => {
+            console.log(response.data.results);
+            setPersonajes(response.data.results);
+            setState(response.data.results);
+            setSpecie(response.data.results);
+            setGender(response.data.results);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handleName = (e) => {
+        console.log(e.target.value);
+        setName(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        obtenerPersonajeByNombre();
+        //cancelamos el evento submit para que no lo vuelva a renderizar
+        e.preventDefault();
+    }
+    const filterState = (e) => {
+        console.log(e.target.value);
+        setState(e.target.value);
+    }
+    const filterSpecie = (e) => {
+        console.log(e.target.value);
+        setSpecie(e.target.value);
+    }
+    const filterGender = (e) => {
+        console.log(e.target.value);
+        setGender(e.target.value);
+    }
+
+    
+    console.log(personajes)
+    return (
+        <div className='hero'>
+            <div className='contenedor row'>
+                <h1 className='text-center pt-4 blanco'>Encuentra a tu personaje favorito!</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="">Ingresa el nombre del personaje</label>
+                    <input type="text" className='form-control' onChange={handleName}/>
+                    <button className='btn btn-primary' type='submit'>Ver Personaje</button>
+                    <h2>Puedes tambien usar los filtros</h2>
+                    <select onChange={filterState} name="" id="">
+                        <option value="" selected disabled>Seleccione Estado</option>
+                        <option id='alive' value='alive'>Vivo</option>
+                        <option id='dead' value='dead'>Muerto</option>
+                        <option id='unknown' value='unknown'>Desconocido</option>
+                    </select>
+                    <select onChange={filterSpecie} name="" id="">
+                    <option value="" selected disabled>Seleccione Especie</option>
+                        <option id='human' value='human'>Humano</option>
+                        <option id='alien' value='alien'>Alien</option>
+                        <option id='humanoid' value='humanoid'>humanoide</option>
+                    </select>
+                    <select onChange={filterGender} name="" id="">
+                        <option value="" selected disabled default>Seleccione Genero</option>
+                        <option id='male' value='male'>Masculino</option>
+                        <option id='female' value='female'>Femenino</option>
+                    </select>
+                    <div className='tip'>
+                    <h3>Consejo 1: recarga la pagina en cada busqueda</h3>
+                    <h3>Consejo 2: si no aparece nada es porque no hay resultados con esas especificaciones</h3>
+                    </div>
+                </form>
+                <br />
+                 {/* apartado del contenido de cada personaje*/}
+
+
+                 {
+                    personajes.length === 0 ? (
+                        <div class="alert alert-danger" role="alert">
+                            No se encontraron resultados
+                        </div>
+                    ) : (
+                        <>
+                            {
+                                personajes.map((personaje, indice) => {
+                                    return(
+                                        <div className=' col-lg-4 col-md-6 col-xs-12 personaje'>
+                                            <div className='margen0'>
+                                                <h2 className='text-center blanco texto'> {personaje.name}</h2>
+                                                <p className='hover '>Estado : {personaje.status} </p>
+                                                <p className=' hover '>Especie : {personaje.species}</p>
+                                                <img className='imgbusqueda marl' src={personaje.image} alt={personaje.name}/>
+                                            </div>
+                                             
+                                        </div>
+                                    )
+                                })
+                            }
+                        </>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
--- src/components/Personajes.jsx
+++ src/components/Personajes.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import Filtros from './Filtros'
 import craneo from '../assets/img/craneo.png'
 import amor from '../assets/img/amor.png'
 import pregunta from '../assets/img/pregunta.png'
@@ -12,7 +11,7 @@ function NavPage({ page, setPage }) {
     <div className="d-flex justify-content-center gap-4 my-5">
       {page > 0 && (
           <button
-              className="btn btn-primary btn-sm"
+              className="btn btn-warning btn-sm fw-bold"
               onClick={() => setPage(page - 1)}>
               <img src={aleft} className="img-fluid arow" alt="aleft"/> 
               PAGINA: {page}
@@ -21,7 +20,7 @@ function NavPage({ page, setPage }) {
 
       {page < 42 && (
           <button
-              className="btn btn-primary btn-sm"
+              className="btn btn-warning btn-sm fw-bold"
               onClick={() => setPage(page + 1)}>
               PAGINA: {page + 1}
               <img src={aright} className="img-fluid arow" alt="aright"/> 
@@ -34,80 +33,84 @@ function NavPage({ page, setPage }) {
 
 export default function Personajes() {
   const [character, setCharacters] = useState([]);
-  const [info, setInfo] = useState({});
   const [page, setPage] = useState(1);
-  const url =  `https://rickandmortyapi.com/api/character?page=${page}`
+  let [pageNumber, updatePageNumber] = useState(1);
+  const [status, updateStatus] = useState("");
+  const [gender, updateGender] = useState("");
+  const [species, updateSpecies] = useState("");
+  let { info, results } = character;
+  const url =  `https://rickandmortyapi.com/api/character/?page=${page}`;
   
-  const obtenerCharacters = (url) => {
+
+  const fetchCharacters = () => {
     axios
       .get(url)
-      .then((data) => {
-        setCharacters(data.data.results);
-        setInfo(data.data.info);
+      .then((response) => {
+        setCharacters(response.data.results);       
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    obtenerCharacters(url);
+    fetchCharacters();
   }, [page]);
- 
-    //console.log(character)
-    return (
-      <div className='container'>    
-          <h1>Rick y Morty Api</h1> 
-           
-                            
-            <div className='row my-5'>
-                {
-                  character.map((item, indice) => {
-                  
-                  return (
-                      
-                      <div className='col-lg-3 col-md-6 col-sm-12 my-4' key={indice}>
-                          <div className="card">
-                                <img src={item.image} className="img-fluid" alt="character"/>    
-                                                    
-                              <div className="detaill">
-                                    <h5>{item.name}</h5> 
-                                    <p>Estado: {item.status}</p> 
-                                    <p>Especie: {item.species}</p>                        
-                                    <p>Género: {item.gender}</p>
-                                    <p>origen: {item.origin.name}</p>
-                                    <p>Ubicación: {item.location.name}</p>                                                                            
-                                </div>
-                          </div>   
-                        
-                          {(() => {
-                                if (item.status === "Dead") {
-                                  return <div className="estatus">
-                                            <img src={craneo} alt="api_rick_and_morty" className='img-fluid' />
-                                         </div>;
-                                  } else if (item.status === "Alive") {
-                                  return <div className=" estatus">
-                                            <img src={amor} alt="api_rick_and_morty" className='img-fluid' />
-                                        </div>;
-                                } else if (item.status === "unknown"){
-                                  return <div className="estatus">
-                                           <img src={pregunta} alt="api_rick_and_morty" className='img-fluid' />
-                                         </div>;
-                                }
-                              })()}
-                                                   
-                      </div>    
-                  )  
-                  }) 
-                }
-              </div>
 
+    return (
+   
+    <div className='container'>    
+        <h1>Rick y Morty Api</h1> 
+       
+            <div className='row my-5'>
+              {
+                  character.map((character, indice) => {
+                  return (
+                          
+                    <div className='col-lg-3 col-md-6 col-sm-12 my-4' key={indice}>
+                        <div className="card">
+                              <img src={character.image} className="img-fluid" alt="character"/>    
+                                                  
+                            <div className="detaill">
+                                  <h5>{character.name}</h5>
+                                  <p>Id: {character.id}</p> 
+                                  <p>Estado: {character.status}</p> 
+                                  <p>Especie: {character.species}</p>                        
+                                  <p>Género: {character.gender}</p>
+                                  <p>origen: {character.origin.name}</p>
+                                  <p>Ubicación: {character.location.name}</p>                                                                            
+                              </div>
+                        </div>   
+                          
+                  {(() => {
+                    if (character.status === "Dead") {
+                      return <div className="estatus">
+                                <img src={craneo} alt="api_rick_and_morty" className='img-fluid' />
+                            </div>;
+                      } else if (character.status === "Alive") {
+                      return <div className=" estatus">
+                                <img src={amor} alt="api_rick_and_morty" className='img-fluid' />
+                            </div>;
+                    } else if (character.status === "unknown"){
+                      return <div className="estatus">
+                              <img src={pregunta} alt="api_rick_and_morty" className='img-fluid' />
+                            </div>;
+                    }
+                  })()}
+                 </div>                          
+                      )
+                    
+                      }) 
+                    }
+        
+                              
+                                                      
+                </div>                       
+                     
               <NavPage page={page} setPage={setPage} />
              
-              
+              </div>
 
-      </div>
-    )
-
-
+    
+  
+  );
 }
+
