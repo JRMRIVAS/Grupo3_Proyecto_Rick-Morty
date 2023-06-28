a91148ef409934701584214de69ca8fbecaba961
luis jimenez <luif1990@hotmail.com>
2023-06-28 13:56:01 -0500

    actualizacion_componente_creadores

--- src/components/Creadores.jsx
+++ src/components/Creadores.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './../assets/css/Lstyles.css'
 
 
 
@@ -7,8 +7,6 @@ import React from 'react'
 export default function Creadores() {
   return (
     <>
-
-    
     <main className='Principal'>
         <section className='container pt-5'>
         <div className='row justify-content-center d-flex'>
@@ -17,7 +15,7 @@ export default function Creadores() {
                 <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
                     <div className='card-body mb-3'>
                       <h5 className='card-title text-center'>Rodrigo Rivas</h5>
-                      <p className='card-text'>desarrollador web</p>
+                      <p className='card-text'>Desarrollador web</p>
                       <p className='mb-0'><i className='bi bi-envelope'></i> jrmrivas21@gmail.com</p>
                       <a className='link-light text-decoration-none' target="_blank" href='https://github.com/JRMRIVAS'><i class="bi bi-github"></i> github.com/JRMRIVAS</a>
                     </div>
@@ -28,8 +26,8 @@ export default function Creadores() {
                 <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
                     <div className='card-body mb-3'>
                       <h5 className='card-title text-center'>Alicia Graciela</h5>
-                      <p className='card-text'>desarrollador web</p>
-                      <p className='mb-0'><i className='bi bi-envelope'></i> correo@gmail.com</p>
+                      <p className='card-text'>Desarrollador web</p>
+                      <p className='mb-0'><i className='bi bi-envelope'></i> aligralinares@gmail.com</p>
                       <a className='link-light text-decoration-none'  target="_blank" href='https://github.com/aligralinares'> <i class="bi bi-github"></i> github.com/aligralinares</a>
                     </div>
                 </div>
@@ -43,7 +41,7 @@ export default function Creadores() {
                 <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
                     <div className='card-body mb-3'>
                       <h5 className='card-title text-center'>Celeste Franco</h5>
-                      <p className='card-text'>desarrollador web</p>
+                      <p className='card-text'>Desarrollador web</p>
                       <p className='mb-0'><i className='bi bi-envelope'></i> mairacolocho2021@gmail.com</p>
                       <a className='link-light text-decoration-none'  target="_blank" href='https://github.com/celestefranco'><i class="bi bi-github"></i> github.com/celestefranco</a>
                     </div>
@@ -54,7 +52,7 @@ export default function Creadores() {
                 <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
                     <div className='card-body mb-3'>
                       <h5 className='card-title text-center'>Luis Jimenez</h5>
-                      <p className='card-text'>desarrollador web</p>
+                      <p className='card-text'>Desarrollador web</p>
                       <p className='mb-0'><i className='bi bi-envelope'></i> luif1990@hotmail.com</p>
                       <a className='link-light text-decoration-none'  target="_blank" href='https://github.com/luisjimenez2508'><i class="bi bi-github"></i> github.com/luisjimenez2508</a>
                     </div>
@@ -69,23 +67,13 @@ export default function Creadores() {
                 <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
                     <div className='card-body mb-3'>
                       <h5 className='card-title text-center'>Eduardo Gomez</h5>
-                      <p className='card-text'>desarrollador web</p>
+                      <p className='card-text'>Desarrollador web</p>
                       <p className='mb-0'><i className='bi bi-envelope'></i> correo@gmail.com</p>
                       <a className='link-light text-decoration-none' target="_blank" href='https://github.com/Eduardosent'><i class="bi bi-github"></i> github.com/Eduardosent</a>
                     </div>
                 </div>
             </div>
-            <div className='col-md-3 mb-4 mx-5'>
-                <div className='Lcard bg-dark text-light' >
-                <img className='card-img-top rounded-circle mx-auto img-box mt-4' src='./src/assets/img/mrmeeseeks.png' alt='Card image cap'/>
-                    <div className='card-body mb-3'>
-                      <h5 className='card-title text-center'>Josue David</h5>
-                      <p className='card-text'>desarrollador web</p>
-                      <p className='mb-0'><i className='bi bi-envelope'></i> correo@gmail.com</p>
-                      <a className='link-light text-decoration-none' target="_blank" href='#'><i class="bi bi-github"></i> github.com/desarrollador</a>
-                    </div>
-                </div>
-            </div>
+
         </div>
         </section>
         </main>
